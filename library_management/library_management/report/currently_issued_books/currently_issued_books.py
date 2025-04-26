d5d073d7eab5db9ad9482b953c28a08ad4ef780d
nadeemohc <nadeemvezhappilly@gmail.com>
2025-04-26 06:32:46 +0200

    Did the report using query builder

--- library_management/library_management/report/currently_issued_books/currently_issued_books.py
+++ library_management/library_management/report/currently_issued_books/currently_issued_books.py
@@ -1,5 +1,7 @@
 import frappe
 from frappe import _
+from frappe.query_builder import DocType
+from frappe.utils import getdate
 
 
 def execute(filters: dict | None = None):
@@ -44,57 +46,36 @@ def get_columns() -> list[dict]:
     ]
 
 
-from frappe.utils import getdate
 
-def get_data(filters=None) -> list[dict]:
-	"""Return data for the report.
+def get_data(filters=None):
+    LibraryTransaction = DocType("Library Transaction")
+    Article = DocType("Article")
+    LibraryMember = DocType("Library Member")
 
-	The report data is a list of rows, with each row being a dict of field values.
-	"""
-	data = []
+    from_date = getdate(filters.get("from_date")) if filters.get("from_date") else None
+    to_date = getdate(filters.get("to_date")) if filters.get("to_date") else None
 
-	query_filters = {
-		"type": "issued",
-		"docstatus": 1
-	}
+    q = (
+        frappe.qb.from_(LibraryTransaction)
+        .join(Article).on(Article.name == LibraryTransaction.article)
+        .join(LibraryMember).on(LibraryMember.name == LibraryTransaction.library_member)
+        .select(
+            Article.article_name.as_("article_name"),
+            Article.image.as_("image"),
+            LibraryMember.full_name.as_("full_name"),
+            LibraryTransaction.date
+        )
+        .where(LibraryTransaction.type == "issued")
+        .where(LibraryTransaction.docstatus == 1)
+    )
 
-	if filters:
-		if filters.get("article"):
-			query_filters["article"] = filters["article"]
-		if filters.get("name"):
-			query_filters["library_member"] = filters["name"]
+    if filters.get("article"):
+        q = q.where(LibraryTransaction.article == filters["article"])
+    if filters.get("name"):
+        q = q.where(LibraryTransaction.library_member == filters["name"])
+    if from_date:
+        q = q.where(LibraryTransaction.date >= from_date)
+    if to_date:
+        q = q.where(LibraryTransaction.date <= to_date)
 
-	# Fetch raw values from filters
-	from_date = filters.get("from_date")
-	to_date = filters.get("to_date")
-
-	# Convert them to datetime.date if provided
-	if from_date:
-		from_date = getdate(from_date)
-	if to_date:
-		to_date = getdate(to_date)
-
-	transactions = frappe.get_all(
-		"Library Transaction",
-		fields=["article", "library_member", "date"],
-		filters=query_filters
-	)
-
-	for txn in transactions:
-		# Date filtering
-		if from_date and txn.date < from_date:
-			continue
-		if to_date and txn.date > to_date:
-			continue
-
-		article = frappe.get_doc("Article", txn.article)
-		member = frappe.get_value("Library Member", txn.library_member, "full_name")
-
-		data.append({
-			"article_name": article.article_name,
-			"image": article.image,
-			"full_name": member,
-			"date": txn.date
-		})
-
-	return data
+    return q.run(as_dict=True)
